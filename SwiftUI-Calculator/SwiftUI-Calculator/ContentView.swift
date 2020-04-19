5dfbd5ccc8a5fba0e6b2fb826a36e99ffed5cef9
Mohamed Salah ElDien <mohamed.salaheldienhamed@vodafone.com>
2020-04-19 14:59:50 +0200

    coda enhancement

--- SwiftUI-Calculator/SwiftUI-Calculator/ContentView.swift
+++ SwiftUI-Calculator/SwiftUI-Calculator/ContentView.swift
@@ -8,41 +8,77 @@
 
 import SwiftUI
 
+struct Constants {
+    static let plus = "+"
+    static let minus = "-"
+    static let multiply = "X"
+    static let devide = "÷"
+    static let equals = "="
+    static let precentage = "%"
+    static let plusMinus = "±"
+    static let clear = "AC"
+
+    static let basicOperators = "=+-÷X"
+    static let extraOperators = "AC±%"
+    static let allOperators = "=+-÷XAC±%"
+}
+
 class GlobalEnvironment: ObservableObject {
 
-    @Published var displayedText = ""
+    @Published var displayedText = "0"
 
     var firstNumber = ""
     var secondNumber = ""
     var equationOperation = ""
 
     func handleButton(_ button: CalculatorButton) {
-        if button.title == "=" {
-            let firstInt = Int(firstNumber) ?? 0
-            let secondInt = Int(secondNumber) ?? 0
-            switch equationOperation {
-            case "+":
-                displayedText = "\(firstInt + secondInt)"
-            case "-":
-                displayedText = "\(firstInt - secondInt)"
-            case "÷":
-                displayedText = "\(firstInt / secondInt)"
-            default:
-                displayedText = "\(firstInt * secondInt)"
-            }
-
-            firstNumber = ""
-            secondNumber = ""
-            equationOperation = ""
-        } else if "+-÷X".contains(button.title) {
+        if button.title == Constants.plusMinus {
+        } else if button.title == Constants.clear {
+            clearData()
+            displayedText = "0"
+        } else if button.title == Constants.equals {
+            calculateResult()
+        } else if Constants.allOperators.contains(button.title) {
             equationOperation = button.title
         } else if equationOperation.isEmpty {
             firstNumber += button.title
             displayedText = firstNumber
-        }  else {
+        } else {
             secondNumber += button.title
             displayedText = secondNumber
         }
+    }
+
+    func clearData() {
+        firstNumber = ""
+        secondNumber = ""
+        equationOperation = ""
+    }
+
+    func calculateResult() {
+        let firstFloat = (firstNumber as NSString).floatValue
+        let secondFloat = (secondNumber as NSString).floatValue
+        var result: Float = 0
+        switch equationOperation {
+        case Constants.plus:
+            result = firstFloat + secondFloat
+        case Constants.minus:
+            result = firstFloat - secondFloat
+        case Constants.devide:
+            result = firstFloat / secondFloat
+        case Constants.multiply:
+            result = firstFloat * secondFloat
+        case Constants.devide:
+            result = firstFloat.truncatingRemainder(dividingBy: secondFloat)
+        default:
+            break
+        }
+        if floor(result) == result {
+            displayedText = "\(Int(result))"
+        } else {
+            displayedText = "\(result)"
+        }
+        clearData()
     }
 }
 
@@ -54,9 +90,9 @@ struct CalculatorButton: Hashable {
         self.title = title
         if Int(title) != nil || title == "." {
             bgColor = Color(.darkGray)
-        } else if "=+-÷X".contains(title) {
+        } else if Constants.basicOperators.contains(title) {
             bgColor = Color(.orange)
-        } else if "AC±%".contains(title) {
+        } else if Constants.extraOperators.contains(title) {
             bgColor = Color(.lightGray)
         }
     }
@@ -67,20 +103,21 @@ struct ContentView: View {
     @EnvironmentObject var env: GlobalEnvironment
 
     let numbers: [[CalculatorButton]] = [
-        [CalculatorButton("AC"), CalculatorButton("±"), CalculatorButton("%"), CalculatorButton("÷")],
-        [CalculatorButton("9"), CalculatorButton("8"), CalculatorButton("7"), CalculatorButton("X")],
-        [CalculatorButton("4"), CalculatorButton("5"), CalculatorButton("6"), CalculatorButton("-")],
-        [CalculatorButton("1"), CalculatorButton("2"), CalculatorButton("3"), CalculatorButton("+")],
-        [CalculatorButton("0"), CalculatorButton("."), CalculatorButton("=")]
+        [CalculatorButton(Constants.clear), CalculatorButton(Constants.plusMinus), CalculatorButton(Constants.precentage), CalculatorButton(Constants.devide)],
+        [CalculatorButton("9"), CalculatorButton("8"), CalculatorButton("7"), CalculatorButton(Constants.multiply)],
+        [CalculatorButton("4"), CalculatorButton("5"), CalculatorButton("6"), CalculatorButton(Constants.minus)],
+        [CalculatorButton("1"), CalculatorButton("2"), CalculatorButton("3"), CalculatorButton(Constants.plus)],
+        [CalculatorButton("0"), CalculatorButton("."), CalculatorButton(Constants.equals)]
     ]
     var body: some View {
         ZStack (alignment: .bottom) {
-            Color.gray.edgesIgnoringSafeArea(.all)
+            Color.black.edgesIgnoringSafeArea(.all)
             VStack (spacing: 15) {
                 HStack {
                     Spacer()
                     Text(env.displayedText)
                         .font(.system(size: 40))
+                        .foregroundColor(Color.white)
                 }.padding()
                 ForEach(numbers, id: \.self) { row in
                     HStack (spacing: 15) {
